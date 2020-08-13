2dfe1a3923940ac1aefd7ce4c6404758fb3b05cc
Peyush Jain <60294264+peyushjain1@users.noreply.github.com>
2020-08-13 18:39:41 +0530

    Upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,16 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+# dockerpath is userid/repo
+ $dockerpath=peyushjain/api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u user id -p password  
+docker tag imageid $dockerpath 
+# Image id mentioned above is id of image generated using docker build command 
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
